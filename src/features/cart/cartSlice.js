5c240bfc37f448b6100cf41e07b6fd2d7ba6a4b2
iamalex <ngashalex8@gmail.com>
2023-06-21 13:11:42 +0300

    redux practise

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+
+const initialState = {
+    cartItems: cartItems,
+    amount:4,
+    total:0,
+    isloading:true
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState
+});
+
+// console.log(cartSlice);
+
+export default cartSlice.reducer;
